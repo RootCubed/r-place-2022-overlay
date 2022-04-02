f01096a6bbe1b9a84eb763c57f087863d2153fdc
Andrea Bettich <andreabettich@users.noreply.github.com>
2022-04-02 20:22:28 +0200

    inital overlay

---
+++ place-left_overlay.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         PlaceCH Overlay Left
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Keep the canvas beautiful!
+// @author       /r/switzerland credits: oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://raw.githubusercontent.com/placeCH/r-place-2022-overlay/main/place-left_overlay.user.js
+// @downloadURL  https://raw.githubusercontent.com/placeCH/r-place-2022-overlay/main/place-left_overlay.user.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://media.discordapp.net/attachments/959089858519924838/959869950103805972/place_switzerland_new_design_overlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
