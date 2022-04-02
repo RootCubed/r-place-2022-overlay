fc51d406317320de3a4f0cf778ffe98b219b9e5a
Andrea Bettich <andreabettich@users.noreply.github.com>
2022-04-02 21:47:19 +0200

    Update place-left_overlay.user.js

--- place-left_overlay.user.js
+++ place-left_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceCH Overlay Left
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Keep the canvas beautiful!
 // @author       /r/switzerland credits: oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
 // @match        https://hot-potato.reddit.com/embed*
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://media.discordapp.net/attachments/959089858519924838/959869950103805972/place_switzerland_new_design_overlay.png";
+            i.src = "https://cdn.discordapp.com/attachments/959089858519924838/959901191662354472/place_switzerland_new_design_overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
