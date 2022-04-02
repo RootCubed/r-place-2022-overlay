7a5071d7ca036636b65a0ec74e4e2252d856199b
Andrea Bettich <andreabettich@users.noreply.github.com>
2022-04-02 21:55:40 +0200

    Update place-left_overlay.user.js

--- place-left_overlay.user.js
+++ place-left_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceCH Overlay Left
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Keep the canvas beautiful!
 // @author       /r/switzerland credits: oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
 // @match        https://hot-potato.reddit.com/embed*
