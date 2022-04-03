f1010d88a877d943ac30292b829b948786f0c014
Liam <41953062+RootCubed@users.noreply.github.com>
2022-04-03 13:50:33 +0200

    Use individual overlays instead of a combined one

--- place_overlay.user.js
+++ place_overlay.user.js
@@ -1,28 +1,38 @@
 // ==UserScript==
 // @name         r/starwars_place Allies
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Keep the canvas beautiful!
-// @author       /r/switzerland credits: oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
+// @author       oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place, overlay list by RootCubed
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/RootCubed/r-place-2022/raw/main/place_overlay.user.js
 // @downloadURL  https://github.com/RootCubed/r-place-2022/raw/main/place_overlay.user.js
 // @grant        none
 // ==/UserScript==
+
+const overlays = [
+    "https://cdn.discordapp.com/attachments/960020069553942538/960020540788199454/overlay_v90.png", // Some others
+    "https://cdn.mirai.gg/tmp/dotted-place-template.png", // Star Wars & Main allies
+    "https://raw.githubusercontent.com/RootCubed/r-place-2022-overlay/main/rplace_switzerland_overlay.png" // Switzerland
+];
+
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        // Load the image
-        const image = document.createElement("img");
-        image.src = "https://raw.githubusercontent.com/RootCubed/r-place-2022-overlay/main/rplace_allies_overlay.png?t=1648980556";
-        image.onload = () => {
-            image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
-        };
-
-        // Add the image as overlay
         const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
         const canvas = camera.querySelector("mona-lisa-canvas");
-        canvas.shadowRoot.querySelector('.container').appendChild(image);
+
+        // Load the image
+        for (let ov of overlays) {
+            const image = document.createElement("img");
+            image.src = ov + "?t=" + Date.now(); // random query parameter to ensure no caching
+            image.onload = () => {
+                image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
+            };
+
+            // Add the image as overlay
+            canvas.shadowRoot.querySelector('.container').appendChild(image);
+        }
 
         // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
